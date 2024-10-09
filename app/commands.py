3bc60c86aeb293abb0d81eb0dd5203127993f5c7
Danny <32425951+dpnny@users.noreply.github.com>
2024-10-09 01:00:38 -0500

    added more commands

--- app/commands.py
+++ app/commands.py
@@ -2,14 +2,78 @@ from core import MoveAction, WaitAction, TeleportAction, MoveUntilObstacleAction
 
 # keyword commands in different file to add an extensive list
 commands = {
+    # Move left
     "move left": MoveAction((-1, 0)),
+    "go left": MoveAction((-1, 0)),
+    "step left": MoveAction((-1, 0)),
+    "walk left": MoveAction((-1, 0)),
+    "turn left": MoveAction((-1, 0)),
+
+    # Move right
     "move right": MoveAction((1, 0)),
+    "go right": MoveAction((1, 0)),
+    "step right": MoveAction((1, 0)),
+    "walk right": MoveAction((1, 0)),
+    "turn right": MoveAction((1, 0)),
+
+    # Move up
     "move up": MoveAction((0, -1)),
+    "go up": MoveAction((0, -1)),
+    "step up": MoveAction((0, -1)),
+    "walk up": MoveAction((0, -1)),
+    "climb up": MoveAction((0, -1)),
+
+    # Move down
     "move down": MoveAction((0, 1)),
-    "open chest": None,  # replace none with open chest function
-    "attack": None,  # replace with attack function
+    "go down": MoveAction((0, 1)),
+    "step down": MoveAction((0, 1)),
+    "walk down": MoveAction((0, 1)),
+
+    # Open chest
+    "open chest": None,
+    "unlock chest": None,
+    "open the chest": None,
+    "unlock the chest": None,
+    "open treasure": None,
+    "open the treasure": None,
+    "unlock treasure": None,  
+    "unlock the treasure": None,
+
+    # Attack
+    "attack": None,
+    "strike": None,
+    "hit": None,
+    "fight": None,
+    "punch": None,
+    "stab": None,
+
+    # Move left all the way
     "move left all the way": MoveUntilObstacleAction((-1, 0)),
+    "go left all the way": MoveUntilObstacleAction((-1, 0)),
+    "step left all the way": MoveUntilObstacleAction((-1, 0)),
+    "walk left all the way": MoveUntilObstacleAction((-1, 0)),
+    "head left all the way": MoveUntilObstacleAction((-1, 0)),
+
+    # Move right all the way
     "move right all the way": MoveUntilObstacleAction((1, 0)),
+    "go right all the way": MoveUntilObstacleAction((1, 0)),
+    "step right all the way": MoveUntilObstacleAction((1, 0)),
+    "walk right all the way": MoveUntilObstacleAction((1, 0)),
+    "head right all the way": MoveUntilObstacleAction((1, 0)),
+
+    # Move up all the way
     "move up all the way": MoveUntilObstacleAction((0, -1)),
+    "go up all the way": MoveUntilObstacleAction((0, -1)),
+    "step up all the way": MoveUntilObstacleAction((0, -1)),
+    "walk up all the way": MoveUntilObstacleAction((0, -1)),
+    "head up all the way": MoveUntilObstacleAction((0, -1)),
+    "climb up all the way": MoveUntilObstacleAction((0, -1)),
+
+    # Move down all the way
     "move down all the way": MoveUntilObstacleAction((0, 1)),
+    "go down all the way": MoveUntilObstacleAction((0, 1)),
+    "step down all the way": MoveUntilObstacleAction((0, 1)),
+    "walk down all the way": MoveUntilObstacleAction((0, 1)),
+    "head down all the way": MoveUntilObstacleAction((0, 1)),
 }
+
